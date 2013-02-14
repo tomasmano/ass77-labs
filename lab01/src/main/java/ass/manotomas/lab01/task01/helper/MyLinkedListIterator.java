a39b91cae95cc7214d30718cf0d4c39bbd5ad4e6
Tomas Mano <tomasmano@gmail.com>
2013-02-14 03:59:17 +0100

    add reversible list lab

---
+++ lab01/src/main/java/ass/manotomas/lab01/task01/helper/MyLinkedListIterator.java
@@ -0,0 +1,51 @@
+package ass.manotomas.lab01.task01.helper;
+
+import java.util.Iterator;
+
+/**
+ *
+ * @author Tomas Mano <tomasmano@gmail.com>
+ */
+public class MyLinkedListIterator<E extends Comparable<E>> implements Iterator<E> {
+
+    private MyLinkedList<E> inner;
+    private Node<E> current;
+
+    public MyLinkedListIterator(MyLinkedList<E> inner, Node<E> current) {
+        this.inner = inner;
+        this.current = current;
+    }
+
+    public boolean hasNext() {
+        return current.hasNext();
+    }
+
+    public E next() {
+        if (current.hasNext()) {
+            E oldCurrent = current.getElement();
+            current = current.getNext();
+            return oldCurrent;
+        }
+        return null;
+    }
+
+    public void remove() {
+        Node<E> oldCurrent = current.getPrevious();
+        if (oldCurrent.getPrevious() instanceof NullNode) {
+            // list contains only one element, so clear it completely
+            inner.clear();
+            return;
+        }
+        current.setPrevious(oldCurrent.getPrevious());
+        oldCurrent.getPrevious().setNext(current);
+    }
+    
+    protected Node<E> nextNode() {
+        if (current.hasNext()) {
+            Node<E> oldCurrent = current;
+            current = current.getNext();
+            return oldCurrent;
+        }
+        return new NullNodeMax<E>();
+    }
+}
---
+++ lab01/src/main/java/ass/manotomas/lab01/task01/helper/Node.java
@@ -0,0 +1,26 @@
+package ass.manotomas.lab01.task01.helper;
+
+/**
+ *
+ * @author Tomas Mano <tomasmano@gmail.com>
+ */
+public interface Node<E extends Comparable<E>> extends Comparable<E> {
+
+    public boolean hasNext();
+    
+    public boolean hasPrevious();
+
+    public E getElement();
+
+    public void setElement(E element);
+
+    public Node<E> getNext();
+
+    public void setNext(Node<E> next);
+
+    public Node<E> getPrevious();
+
+    public void setPrevious(Node<E> previous);
+
+    public int compareTo(E o);
+}
